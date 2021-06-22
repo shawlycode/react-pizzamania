5d3caf09984b7ec0cfb95561bc3ef49bac1a3765
shawlycode <orlandoshawly>
2021-06-22 17:45:53 +0000

    add navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar.css'
+import './Navbar.css'
 
 function Navbar() {
     return (
+
         <nav className="navbar navbar-default narvbar-pizza">
             <div className="container-fluid">
-                {/* <!-- Brand and toggle get grouped for better mobile display --> */}
                 <div className="navbar-header">
                     <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                         <span className="sr-only">Toggle navigation</span>
@@ -15,8 +15,6 @@ function Navbar() {
                     </button>
                     <a className="navbar-brand navbar-pizzamania" href="#">Pizzamania</a>
                 </div>
-
-                {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
                     <ul className="nav navbar-nav navbar-right navbar-home">
@@ -28,7 +26,6 @@ function Navbar() {
                 </div>
             </div>
         </nav>
-
     );
 }
 
