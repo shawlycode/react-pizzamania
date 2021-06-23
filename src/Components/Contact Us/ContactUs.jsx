23231f757bc55645b03c749f8ea1434dc8412871
shawlycode <orlandoshawly>
2021-06-23 17:09:46 +0000

    add components

---
+++ src/Components/Contact Us/ContactUs.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './ContactUs.css'
+
+function ContactUs() {
+    return (
+        <div className="container-form">
+            <div className="contact-form">
+                <h4 className="text-3">CONTACT FORM</h4>
+                <div className="row-fields">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quas atque ipsum tenetur? Esse, aliquam.</p>
+                </div>
+
+            </div>
+            <div className="contact-form1">
+                <h5>Menu</h5>
+                <div className="row-fields">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quas atque ipsum tenetur? Esse, aliquam.</p>
+                    <label htmlFor="">Menu</label>&nbspn;<input type="text" placeholder/>
+                </div>
+
+            </div>
+            <div className="contact-form1">
+               
+                <div className="row-fields">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quas atque ipsum tenetur? Esse, aliquam.</p>
+                </div>
+
+            </div>
+            <div className="contact-form1">
+                
+                <div className="row-fields">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quas atque ipsum tenetur? Esse, aliquam.</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ContactUs
---
+++ src/Components/PizzaTypes/PizzaTypes.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './PizzaTypes.css'
+import pizz from '../images/image-1.jpg'
+
+function PizzaTypes() {
+    return (
+        <div className="pizzatypes-content">
+            <h2  className="text-2" style={{textAlign:'center'}}>Pizza Types</h2>
+            <hr />
+            <div className="row">
+                <div className="col-md-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src={pizz} className="card-img-top" alt="pizza" />
+                        <div className="card-body">
+                            <h5 className="card-title">Peperoni Pizza</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est maiores a ratione corrupti beatae, consequatur neque dolor quam cumque quaerat.</p>
+                            <a href="#" className="btn btn-primary">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src={pizz} className="card-img-top" alt="pizza" />
+                        <div className="card-body">
+                            <h5 className="card-title">Peperoni Pizza</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est maiores a ratione corrupti beatae, consequatur neque dolor quam cumque quaerat.</p>
+                            <a  href="#" className="btn btn-primary">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src={pizz} className="card-img-top" alt="pizza" />
+                        <div className="card-body">
+                            <h5 className="card-title">Peperoni Pizza</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est maiores a ratione corrupti beatae, consequatur neque dolor quam cumque quaerat.</p>
+                            <a href="#" className="btn btn-primary">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src={pizz} className="card-img-top" alt="pizza" />
+                        <div className="card-body">
+                            <h5 className="card-title">Peperoni Pizza</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est maiores a ratione corrupti beatae, consequatur neque dolor quam cumque quaerat.</p>
+                            <a href="#" className="btn btn-primary">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    );
+
+}
+export default PizzaTypes;
