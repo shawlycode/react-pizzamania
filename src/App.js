777475b4a784b4fef70ae7c66108077822b3c84e
shawlycode <orlandoshawly>
2021-06-23 17:11:27 +0000

    added components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import About from './Components/About/About';
 import Carousel from './Components/Carousel/Carousel';
+import PizzaTypes from './Components/PizzaTypes/PizzaTypes';
+import ContactUs from './Components/Contact Us/ContactUs';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Navbar />
       <Carousel />
       <About />
+      <PizzaTypes />
+      <ContactUs />
     </div>
   );
 }
