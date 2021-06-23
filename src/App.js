3fdcb9b9bac73027f9c059d78d3099ddfcb1103f
shawlycode <orlandoshawly>
2021-06-23 10:34:49 +0000

    removed exported/returned components from App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Carousel from './Components/Carousel/Carousel';
-import About from './Components/About/About';
-import PizzaTypes from './Components/PizzaTypes/PizzaTypes';
-import Footer from './Components/Footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-    <Navbar />
-    <Carousel />
-    <PizzaTypes />
-    <About />
-    <Footer />
+   
     </div>
   );
 }
