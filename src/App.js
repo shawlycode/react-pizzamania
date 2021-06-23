6aa4b446e1e558ccd102492199d00e9f4c33b1d8
shawlycode <orlandoshawly>
2021-06-23 13:53:35 +0000

    added returned components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Components/About/About';
+import Carousel from './Components/Carousel/Carousel';
 
 
 function App() {
   return (
     <div className="App">
-   
+      <Navbar />
+      <Carousel />
+      <About />
     </div>
   );
 }
